fb3309cf5bdbb1c315070687fd65c5c154a46f08
Otavio Augusto Reis Almeida <otavio.almeida@pwi.com.br>
2025-01-15 11:02:57 -0300

    feat : adicionando o "Create" de produtos

--- SalesWebMVC/1 - Application/Controllers/ProductController.cs
+++ SalesWebMVC/1 - Application/Controllers/ProductController.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NuGet.Protocol.Plugins;
 using SalesWebMVC._3___Data.Entity;
 using SalesWebMVC.Models.DTO;
+using SalesWebMVC.Models.Exceptions;
 using SalesWebMVC.UnitOfWork;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SalesWebMVC._1___Application.Controllers
 {
@@ -27,6 +31,34 @@ namespace SalesWebMVC._1___Application.Controllers
             return View(productsDTO);
         }
 
+        public ActionResult<ProductDTO> Create()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult<ProductDTO> Create(ProductDTO productDTO)
+        {
+            try
+            {
+                if (productDTO is { DsNome: "", Quantity : 0})
+                {
+                    return RedirectToAction(nameof(Error), new { message = "Seller is not found" });
+                }
+
+                var product = _mapper.Map<ProductEntity>(productDTO);
+
+                _uof._Product.Post(product);
+                _uof.Commit();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Product is not found" });
+            }
+        }
 
     }
 }
